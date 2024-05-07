2b1fdba4b10b6186b8771a188205488999fddf79
matt2ology <34324060+matt2ology@users.noreply.github.com>
2024-05-07 04:26:48 -0700

    init commit

---
+++ app.py
@@ -0,0 +1,3 @@
+from firebase_admin import credentials  # Firebase Admin SDK credentials
+from firebase_admin import db  # Firebase Realtime Database
+from mfrc522 import SimpleMFRC522  # RFID library for the MFRC522 module
