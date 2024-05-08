94caacb1c246e77645a8cbbf028029eba490ce7d
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2024-05-08 00:04:56 -0700

    Added app.py

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
-from firebase_admin import credentials  # Firebase Admin SDK credentials
-from firebase_admin import db  # Firebase Realtime Database
-from mfrc522 import SimpleMFRC522  # RFID library for the MFRC522 module
+import sys
+
+
+def launch_gui():
+    """Import and launch your GUI module"""
+    # import gui.gui # Import gui module from gui package in src directory
+    # gui.gui.main() # Call main function in gui module to start the GUI
+
+
+def launch_cli():
+    """Import and launch your CLI module
+    """
+    import src.cli  # Import cli module from src package
+    src.cli.main()  # Call main function in cli module to start the CLI
+
+
+def main():
+    """Check command-line arguments or user input to
+    determine the interface to launch
+    """
+    print("Launching...")
+    if len(sys.argv) > 1 and sys.argv[1] == "--cli":
+        launch_cli()
+    else:
+        launch_gui()
+
+
+if __name__ == "__main__":
+    main()
